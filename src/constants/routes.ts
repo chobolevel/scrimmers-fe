a132d436033418fc5dd3f946e63f08a8e0e8927d
chobolevel <rodaka123@naver.com>
2024-12-27 10:29:50 +0900

    Fix: invalid api path issue

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,5 +1,5 @@
 export enum ApiV1Paths {
-  LOGIN = '/api/v1/login',
-  LOGOUT = '/api/v1/logout',
-  REISSUE = '/api/v1/reissue',
+  LOGIN = '/api/v1/auth/login',
+  LOGOUT = '/api/v1/auth/logout',
+  REISSUE = '/api/v1/auth/reissue',
 }
